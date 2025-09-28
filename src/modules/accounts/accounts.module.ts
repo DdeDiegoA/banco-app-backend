0422c8e5d9e2168bd1434c62c6be1a8b60b71389
Diego Arenas <diegoarenas@Mac-mini-de-Diego.local>
2025-09-28 16:13:46 -0500

    feat(accounts): integrate TypeORM into accounts module
    - Import TypeOrmModule for Account and Client entities

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { Client } from '../users/entities/client.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Account, Client])],
   providers: [AccountsService],
   controllers: [AccountsController],
 })
