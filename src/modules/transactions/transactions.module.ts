f4766ea48de3d5b297111f2e297c58123a73f174
Diego Arenas <diegoarenas@Mac-mini-de-Diego.local>
2025-09-28 16:15:47 -0500

    feat(transactions): integrate TypeORM into transactions module
    - Import TypeOrmModule for Transaction, LedgerEntry, and Account entities

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { LedgerEntry } from './entities/ledger-entry.entity';
+import { Account } from '../accounts/entities/account.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transaction, LedgerEntry, Account])],
   providers: [TransactionsService],
-  controllers: [TransactionsController]
+  controllers: [TransactionsController],
 })
 export class TransactionsModule {}
